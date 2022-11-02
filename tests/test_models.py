cd1a24bf888c3682a602c20520e2284a33a06ac9
Howitt <Annabel.Howitt@stfc.ac.uk>
2022-11-02 10:03:20 +0000

    new tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,3 +29,54 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_max_integers():
+    """Test that max function works for an array of positive integers."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([3, 4])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_integers():
+    """Test that min function works for an array of positive integers."""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([3, 4])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+    
+ def test_daily_max_negative():
+    """Test that max function works for an array of negative integers."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[-1, -2],
+                           [-3, -4],
+                           [-5, -6]])
+    test_result = np.array([-3, -4])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_negative():
+    """Test that min function works for an array of negative integers."""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[-1, -2],
+                           [-3, -4],
+                           [-5, -6]])
+    test_result = np.array([-3, -4])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)   
